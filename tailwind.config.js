6d2c08263eb83258af286ac2408cb53c3c6b1fd4
elsacodesid <elsacodesid@gmail.com>
2024-06-14 10:31:12 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
         "secondary-400": "#FFCD58",
         "secondary-500": "#FFC132"
       }, 
+      backgroundImage: (theme) ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)"
+      })
     },
   },
   plugins: [],
