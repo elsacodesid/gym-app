f36cb33218f4e4e8a3cef85d1d1531e385914b39
elsacodesid <elsacodesid@gmail.com>
2024-06-16 09:52:37 +0700

    update

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,15 @@
 import { useEffect } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import Logo from '@/assets/Logo.png';
+import Link from './Link';
+import { SelectedPage } from '@/share/types';
 
-type Props = {};
+type Props = {
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = 'flex items-center justify-between';
 
     return (
@@ -18,10 +23,10 @@ const Navbar = (props: Props) => {
                         {/* Right Side */}
                         <div className={`${flexBetween} w-full`}>
                             <div className={`${flexBetween} gap-8 text-sm`}>
-                                <p>Home</p>
-                                <p>Benefits</p>
-                                <p>Our Classes</p>
-                                <p>Contact Us</p>
+                                <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
                             </div>
                             <div className={`${flexBetween} gap-8`}>
                                 <p>Sign In</p>
