35cf071b3fb1641450896514546e36d9bde1156d
elsacodesid <elsacodesid@gmail.com>
2024-06-16 10:40:02 +0700

    update

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,16 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import Logo from '@/assets/Logo.png';
 import Link from './Link';
 import { SelectedPage } from '@/share/types';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 type Props = {
     selectedPage: SelectedPage;
-    setSelectedPage: (value: SelectedPage) => void
+    setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = 'flex items-center justify-between';
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
+    const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
 
     return (
         <nav>
@@ -21,18 +24,44 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                         <img alt="logo" src={Logo} />
 
                         {/* Right Side */}
-                        <div className={`${flexBetween} w-full`}>
-                            <div className={`${flexBetween} gap-8 text-sm`}>
-                                <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                                <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                                <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                                <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexBetween} w-full`}>
+                                <div className={`${flexBetween} gap-8 text-sm`}>
+                                    <Link
+                                        page="Home"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page="Benefits"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page="Our Classes"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page="Contact Us"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                </div>
+                                <div className={`${flexBetween} gap-8`}>
+                                    <p>Sign In</p>
+                                    <button>Become a Member</button>
+                                </div>
                             </div>
-                            <div className={`${flexBetween} gap-8`}>
-                                <p>Sign In</p>
-                                <button>Become a Member</button>
-                            </div>
-                        </div>
+                        ) : (
+                            <button
+                            className="rounded-full bg-secondary-500 p-2"
+                            onClick={() => setIsMenuToggled(!isMenuToggled)}
+                            >
+                                <Bars3Icon className="h-6 w-6 text-white" />
+
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
