246c073a7986178cfc16ef17c52759a7278c1102
elsacodesid <elsacodesid@gmail.com>
2024-06-18 15:36:24 +0700

    update contactform

--- src/scenes/ContactUs/index.tsx
+++ src/scenes/ContactUs/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-    const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
+    const inputStyles = `mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
     const {
         register,
         trigger,
@@ -62,11 +62,71 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                             visible: { opacity: 1, y: 0 },
                         }}
                     >
-                        <form target="_blank" onSubmit={onSubmit} method="POST" action="https://formsubmit.co/elsacodesid@gmail.com">
-                        <input className={inputStyles} type="text" placeholder="NAME" {...register("name", {
-                            required: true,
-                            maxLength: 100,
-                        })}></input>
+                        <form
+                            target="_blank"
+                            onSubmit={onSubmit}
+                            method="POST"
+                            action="https://formsubmit.co/elsacodesid@gmail.com"
+                        >
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="NAME"
+                                {...register('name', {
+                                    required: true,
+                                    maxLength: 100,
+                                })}
+                            />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === 'required' &&
+                                        'This field is required'}
+                                    {errors.name.type === 'maxLength' &&
+                                        'Max length is 100 characters'}
+                                </p>
+                            )}
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="EMAIL"
+                                {...register('email', {
+                                    required: true,
+                                    pattern:
+                                        /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                })}
+                            />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === 'required' &&
+                                        'This field is required'}
+                                    {errors.name.type === 'pattern' &&
+                                        'Invalid e-mail address'}
+                                </p>
+                            )}
+                            <textarea
+                                className={inputStyles}
+                                rows={4}
+                                cols={50}
+                                placeholder="MESSAGE"
+                                {...register('message', {
+                                    required: true,
+                                    maxLength: 2000,
+                                })}
+                            />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === 'required' &&
+                                        'This field is required'}
+                                    {errors.name.type === 'maxLength' &&
+                                        'Max length is 2000 characters'}
+                                </p>
+                            )}
+                            <button
+                                type="submit"
+                                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                            >
+                                SUBMIT
+                            </button>
                         </form>
                     </motion.div>
                 </div>
