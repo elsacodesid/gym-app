67c0ddf2e8e439952a91414c56c4a51ad535a88f
elsacodesid <elsacodesid@gmail.com>
2024-06-18 10:12:04 +0700

    update benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,11 +1,30 @@
 import HText from '@/shared/HText';
-import { SelectedPage } from '@/shared/types';
+import { BenefitType, SelectedPage } from '@/shared/types';
 import {
     HomeModernIcon,
     UserGroupIcon,
     AcademicCapIcon,
-} from '@heroicons/react';
+} from '@heroicons/react/24/outline';
 import { motion } from 'framer-motion';
+import Benefit from './Benefit';
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6 " />,
+        title: "State of the Art Facilities",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque consequatur explicabo quisquam molestias non, debitis minus tempore, odit dolor quasi praesentium illo, dolore iure voluptatum ducimus officiis voluptatem aliquam voluptates!"
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6 " />,
+        title: "100's of Diverse Classes",
+        description: "Quisquam molestias non, debitis minus tempore, odit dolor quasi praesentium illo, dolore iure voluptatum ducimus officiis voluptatem aliquam voluptates!"
+    },
+    {
+        icon: <AcademicCapIcon className="h-6 w-6 " />,
+        title: "Expert and Pro Trainers",
+        description: "Tempore, odit dolor quasi praesentium illo, dolore iure voluptatum ducimus officiis voluptatem aliquam voluptates!"
+    },
+]
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -24,6 +43,17 @@ const Benefits = ({ setSelectedPage }: Props) => {
                   <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.</p>
                 </div>
                 {/* Benefits */}
+                <div className="md:flex items-center justify-between mt-8 gap-5">
+                    {benefits.map((benefit: BenefitType) => (
+                        <Benefit
+                            key={benefit.title}
+                            icon={benefit.icon}
+                            title={benefit.title}
+                            description={benefit.description}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    ))}
+                </div>
                 
             </motion.div>
         </section>
